515b556865d72068fa0c9b7609409aca02a4fe79
Marcin <ksiazek.marcin7@gmail.com>
2025-07-23 16:27:49 +0200

    initial commit

---
+++ cypress-01/README.md.txt
@@ -0,0 +1,7 @@
+# QA Automation – Cypress Tests
+
+Repozytorium z testami automatycznymi end-to-end napisanymi w Cypressie.
+
+## 🔧 Stack
+- Cypress
+- JavaScript (Node.js)
---
+++ cypress-01/cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,6 @@
+describe('Fundamentals test', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/fundamentals')
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundsdmentals/i)
+  })
+})
