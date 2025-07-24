d593875410b7aedbe61c6ed6dfd44018769961ba
Marcin <ksiazek.marcin7@gmail.com>
2025-07-24 15:07:05 +0200

    added new cypress tests

--- cypress-01/cypress/e2e/fundamentals.cy.js
+++ cypress-01/cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,22 @@
 describe('Fundamentals test', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundsdmentals/i)
+
+   beforeEach(() => {
+   cy.visit('/fundamentals')  // otwieraj stronę fundamentals przed każdym testem
   })
+
+  it('contains correct header text', () => {
+    
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+  })
+
+  it('Accordion works correctly', () => {
+   
+    cy.contains(/Your tests will exist in a describe block./i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"] ').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  })
+
 })
+
